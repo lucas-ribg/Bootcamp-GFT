3f78c1810b53a77396578195288e30755d452bf7
Lucas Ribeiro <lucas123.r.g@gmail.com>
2022-01-29 14:52:40 -0300

    implementando metodo e documentando a classe

--- Desafio-POO/src/ribeiro/lucas/models/Course.java
+++ Desafio-POO/src/ribeiro/lucas/models/Course.java
@@ -1,22 +1,42 @@
 package ribeiro.lucas.models;
 
+/**
+ * Represent courses
+ */
 public class Course extends Content{
     private int workload;
 
+    /**
+     * Course constructor
+     * @param title         course title
+     * @param description   course description
+     * @param workload      course workload
+     */
     public Course(String title, String description, int workload) {
         super(title, description);
         this.workload = workload;
     }
 
+    /**
+     * calculates XP
+     * @return XP
+     */
     @Override
-    public void calcularXp() {
-
+    public double calculateXp() {
+        return XP_PADRAO + 20d;
     }
 
+    /**
+     * Workload getter
+     * @return workload
+     */
     public int getWorkload() {
         return workload;
     }
 
+    /**
+     * @return object details
+     */
     @Override
     public String toString() {
         return "Course{" +
