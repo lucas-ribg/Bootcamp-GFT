4ba9058af4c8e4c39a62226564b37c06819c91ec
Lucas Ribeiro <lucas123.r.g@gmail.com>
2022-01-29 09:50:21 -0300

    criando a classe de mentoria

--- Desafio-POO/src/ribeiro/lucas/models/Mentorship.java
+++ Desafio-POO/src/ribeiro/lucas/models/Mentorship.java
@@ -1,2 +1,30 @@
-package ribeiro.lucas.models;public class Mentorship {
+package ribeiro.lucas.models;
+
+import java.time.LocalDateTime;
+
+public class Mentorship extends Content{
+    private LocalDateTime mentorshipDate;
+
+    public Mentorship(String title, String description, LocalDateTime mentorshipDate) {
+        super(title, description);
+        this.mentorshipDate = mentorshipDate;
+    }
+
+    @Override
+    public void calcularXp() {
+
+    }
+
+    public LocalDateTime getMentorshipDate() {
+        return mentorshipDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentorship{" +
+                "title=" + this.getTitle() +
+                "description=" + this.getDescription() +
+                "dataDaMentoria=" + mentorshipDate +
+                '}';
+    }
 }
