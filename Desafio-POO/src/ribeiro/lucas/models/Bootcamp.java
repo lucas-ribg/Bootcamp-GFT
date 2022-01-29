87297c6d047da3e1ca7b1d179b5438caf7cbd08a
Lucas Ribeiro <lucas123.r.g@gmail.com>
2022-01-29 09:57:43 -0300

    criando classe de bootcamp

--- Desafio-POO/src/ribeiro/lucas/models/Bootcamp.java
+++ Desafio-POO/src/ribeiro/lucas/models/Bootcamp.java
@@ -1,2 +1,59 @@
-package ribeiro.lucas.models;public class Bootcamp {
+package ribeiro.lucas.models;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+    private String name;
+    private String description;
+    private LocalDateTime intitalDate;
+    private LocalDateTime finalDate;
+    private Set<Devs> subscribedDevs = new LinkedHashSet<>();
+    private Set<Content> bootcampContent = new LinkedHashSet<>();
+
+    public Bootcamp(String name, String description, LocalDateTime intitalDate, LocalDateTime finalDate) {
+        this.name = name;
+        this.description = description;
+        this.intitalDate = intitalDate;
+        this.finalDate = finalDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(name, bootcamp.name) && Objects.equals(description, bootcamp.description) && Objects.equals(intitalDate, bootcamp.intitalDate) && Objects.equals(finalDate, bootcamp.finalDate) && Objects.equals(subscribedDevs, bootcamp.subscribedDevs) && Objects.equals(bootcampContent, bootcamp.bootcampContent);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, intitalDate, finalDate, subscribedDevs, bootcampContent);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getIntitalDate() {
+        return intitalDate;
+    }
+
+    public LocalDateTime getFinalDate() {
+        return finalDate;
+    }
+
+    public Set<Devs> getSubscribedDevs() {
+        return subscribedDevs;
+    }
+
+    public Set<Content> getBootcampContent() {
+        return bootcampContent;
+    }
 }
