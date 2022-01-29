f4968cc2860fc9a1c590bfde303a23a78f955dbd
Lucas Ribeiro <lucas123.r.g@gmail.com>
2022-01-29 14:55:23 -0300

    implementando metodo e documentando classe

--- Desafio-POO/src/ribeiro/lucas/models/Mentorship.java
+++ Desafio-POO/src/ribeiro/lucas/models/Mentorship.java
@@ -2,23 +2,43 @@ package ribeiro.lucas.models;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents mentorships
+ */
 public class Mentorship extends Content{
     private LocalDateTime mentorshipDate;
 
+    /**
+     * Mentorship constructor
+     * @param title             mentorship title
+     * @param description       mentorship description
+     * @param mentorshipDate    mentorship date
+     */
     public Mentorship(String title, String description, LocalDateTime mentorshipDate) {
         super(title, description);
         this.mentorshipDate = mentorshipDate;
     }
 
+    /**
+     * Calculates XP
+     * @return XP
+     */
     @Override
-    public void calcularXp() {
-
+    public double calculateXp() {
+        return XP_PADRAO + 30d;
     }
 
+    /**
+     * Mentorship date getter
+     * @return date
+     */
     public LocalDateTime getMentorshipDate() {
         return mentorshipDate;
     }
 
+    /**
+     * @return object details
+     */
     @Override
     public String toString() {
         return "Mentorship{" +
