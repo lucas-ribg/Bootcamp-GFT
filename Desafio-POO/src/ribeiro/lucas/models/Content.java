d9f57bd6640cfe237b9393f653057188e83a3517
Lucas Ribeiro <lucas123.r.g@gmail.com>
2022-01-29 10:25:23 -0300

    documentando a classe

--- Desafio-POO/src/ribeiro/lucas/models/Content.java
+++ Desafio-POO/src/ribeiro/lucas/models/Content.java
@@ -1,24 +1,46 @@
 package ribeiro.lucas.models;
 
+/**
+ * Models content of bootcamps
+ */
 public abstract class Content {
     private String title;
     private String description;
 
+    /**
+     * Content constructor
+     * @param title content title
+     * @param description content description
+     */
     public Content(String title, String description) {
         this.title = title;
         this.description = description;
     }
 
-    public abstract void calcularXp();
+    /**
+     * calculates XP
+     */
+    public abstract void calculateXp();
 
+    /**
+     * Content title getter
+     * @return title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Content description getter
+     * @return description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * @return content details
+     */
     @Override
     public String toString() {
         return "Content{" +
