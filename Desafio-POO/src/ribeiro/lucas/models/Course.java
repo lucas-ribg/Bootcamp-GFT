87d941cbc982574a1b65aa7640154f7ced764052
Lucas Ribeiro <lucas123.r.g@gmail.com>
2022-01-29 09:49:56 -0300

    criando a classe curso

--- Desafio-POO/src/ribeiro/lucas/models/Course.java
+++ Desafio-POO/src/ribeiro/lucas/models/Course.java
@@ -1,2 +1,28 @@
-package ribeiro.lucas.models;public class Course {
+package ribeiro.lucas.models;
+
+public class Course extends Content{
+    private int workload;
+
+    public Course(String title, String description, int workload) {
+        super(title, description);
+        this.workload = workload;
+    }
+
+    @Override
+    public void calcularXp() {
+
+    }
+
+    public int getWorkload() {
+        return workload;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "title=" + this.getTitle() +
+                "description=" + this.getDescription() +
+                "workload=" + workload +
+                '}';
+    }
 }
